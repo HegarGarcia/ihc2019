8edde24d08c09b50e62121da0ae23af509548515
Hegar Garcia <hegargarcia@gmail.com>
2019-09-08 22:31:11 -0500

    Adding webpack config for bundling

---
+++ p1_project/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  devtool: "source-map",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    modules: ["node_modules", "core"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    stats: "minimal",
+    port: 9000,
+    disableHostCheck: true
+  }
+};
